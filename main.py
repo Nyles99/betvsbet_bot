b9170086d61efc88ee5498945d4527dae8fb6fcc
Nikita Molchanov <nikita@PARCNEW>
2025-06-05 16:30:22 +0300

    middleware

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
-import logging
-import asyncio
-from aiogram import Bot, Dispatcher, types
+from contextlib import asynccontextmanager
 
+from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from aiogram.filters import CommandStart
-
-
-API_TOKEN = '7634755610:AAHPl9fRym-y9DEL1haYqrLOJRjdxkh1Xe8'
-ADMIN_ID = 831040832  # замените на ваш ID
-
-logging.basicConfig(level=logging.INFO)
-
-bot = Bot(token=API_TOKEN)
-
-dp = Dispatcher()
-
-@dp.message(CommandStart())
-async def start_cmd(message: types.Message):
-    await message.answer('Это была команда старт')
-
-
-
-
-async def main():
-    await dp.start_polling(bot)
-    
-asyncio.run(main())
+from models import init_db 
+import requests as rq
