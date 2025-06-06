229deba9bd2ebbfe2312a7ea6faa6d07fb74ccc3
Nikita Molchanov <nikita@PARCNEW>
2025-06-06 15:17:30 +0300

    создание и проверка нового пользователя

--- main.py
+++ main.py
@@ -6,3 +6,27 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from models import init_db 
 import requests as rq
+
+
+@asynccontextmanager
+async def lifespan(app_: FastAPI):
+    await init_db()
+    print('Bot is ready')
+    yield
+
+
+app = FastAPI(title="To Do App", lifespan=lifespan)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/api/tours/{tg_id}")
+async def tours(tg_id: int):
+    user = await rq.add_user(tg_id)
---
+++ requests.py
@@ -0,0 +1,17 @@
+from sqlalchemy import select, update, delete, func
+from models import async_session, User, Tour
+from pydantic import BaseModel, ConfigDict
+from typing import List
+
+
+async def add_user(tg_id):
+    async with async_session() as session:
+        user = await session.scalar(select(User).where(User.tg_id == tg_id))
+        if user:
+            return user
+        
+        new_user = User(tg_id=tg_id)
+        session.add(new_user)
+        await session.commit()
+        await session.refresh(new_user)
+        return new_user
